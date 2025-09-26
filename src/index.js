d894f71e3aff3f63cde928b5a64df5da3559d5f3
Ravon K. Henson <ravonhenson@gmail.com>
2025-09-26 10:54:03 -0400

    Fixes to package-lock.json to correct building project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const { app, bgioServer} = require("./app");
 const PORT = process.env.PORT || 4000;
 
+// API routes for user management, leaderboards, etc
 app.listen(PORT, () => {
   console.log(`Server at http://localhost:${PORT}`);
 });
 
+// Board game io server that powers Grow N' Flow
 bgioServer.run(8000, () => {
   console.log(`Boardgame.io server at http://localhost:8000`);
 })
